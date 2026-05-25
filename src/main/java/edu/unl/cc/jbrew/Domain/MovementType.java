f4bc3a9a9a0d5aca74f31ae73c506cb93bbca697
Diyer Torres <novaforgestudio.apps@gmail.com>
2026-05-25 09:33:32 -0500

    Implementación de las clases, atributos y métodos del sistema de gestión de inventario

---
+++ src/main/java/edu/unl/cc/jbrew/Domain/MovementType.java
@@ -0,0 +1,6 @@
+package edu.unl.cc.jbrew.Domain;
+
+public enum MovementType {
+    SALE,
+    RESTOCK
+}
---
+++ src/main/java/edu/unl/cc/jbrew/Domain/ProductStatus.java
@@ -0,0 +1,9 @@
+package edu.unl.cc.jbrew.Domain;
+
+public enum ProductStatus {
+    FULL,
+    EMPTY,
+    LOW_STOCK,
+    AVAILABLE,
+    OUT_OF_STOCK
+}
